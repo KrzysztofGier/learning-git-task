d54ff42c9721793f569f37f348ab9f634740e92f
Krzysztof Gierczak <krzysztof.gierczak91@gmail.com>
2022-04-26 22:57:50 +0200

    Dodanie slownika i petli

---
+++ Zakupy.py
@@ -0,0 +1,8 @@
+shopping_dict={
+    "piekarnia": ['chleb', 'pączek', 'bułki'],
+    "warzywniak": ['marchew', 'seler', 'rukola']
+}
+
+for i in shopping_dict:
+    print("Idę do "+i+", kupuję tu następujące rzeczy" +str(shopping_dict[i]))
+
